e4e67b2f0ada35f6014582a864bac85a52a1ba4a
Ocelot <100012348+billycwong19@users.noreply.github.com>
2022-08-04 10:39:30 -0500

    testing in insomnia to receive express data

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-console.log('server..')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const PORT = process.env.PORT || 3002
+
+const app = express()
+
+app.get('/api/users', (req, res) => {
+    res.status(200).json({message: 'welcome to the support desk api'})
+})
+
+app.listen(PORT, () => console.log(`Server up and running on PORT: ${PORT}`))
