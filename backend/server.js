9f1386b27198a772bd52b0d774881470cd3dd636
Ocelot <100012348+billycwong19@users.noreply.github.com>
2022-08-04 13:29:26 -0500

    added front end and completed backed with jwt

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,26 @@
 const express = require('express')
+const colors = require('colors')
 const dotenv = require('dotenv').config()
+const { errorHandler } = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
 const PORT = process.env.PORT || 3002
+
+// connect to database
+
+connectDB()
 
 const app = express()
 
-app.get('/api/users', (req, res) => {
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/', (req, res) => {
     res.status(200).json({message: 'welcome to the support desk api'})
 })
+
+// Routes
+app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Server up and running on PORT: ${PORT}`))
